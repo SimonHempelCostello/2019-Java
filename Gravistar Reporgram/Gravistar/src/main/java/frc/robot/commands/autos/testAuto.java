a0a109a62973b9b52896e02928dd8f5940b934f3
Simon Hempel <hempel-costello.simon20@libertycommon.org>
2019-12-27 16:50:24 -0700

    Initial steps in setting up 2 hatch auto

--- Gravistar Reporgram/Gravistar/src/main/java/frc/robot/commands/autos/testAuto.java
+++ Gravistar Reporgram/Gravistar/src/main/java/frc/robot/commands/autos/testAuto.java
@@ -10,16 +10,26 @@ package frc.robot.commands.autos;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 import frc.robot.commands.controls.DeadReackonDrive;
 import frc.robot.commands.controls.SetRobotOdometryPostion;
 import frc.robot.tools.controlLoops.CascadingPIDTurn;
 import frc.robot.tools.controlLoops.PurePursuitController;
-
 public class testAuto extends CommandGroup {
 
   public testAuto() {
     addSequential(new PurePursuitController(Robot.pathlist.test1Path, 0.8, 4.8, true, true));
-    addSequential(new CascadingPIDTurn(120, 0.3, 0.01, 0));
-    addSequential(new PurePursuitController(Robot.pathlist.test2Path, 0.8, 4.8, true, true));
+    addSequential(new CascadingPIDTurn(120, 0.7, 0.02, 0));
+    addSequential(new WaitCommand(0.25));
+    addSequential(new PurePursuitController(Robot.pathlist.test2Path, 1.2, 4.8, true, true));
+    addSequential(new PurePursuitController(Robot.pathlist.test3Path, 1.2, 4.0, true, true));
+    addSequential(new WaitCommand(0.25));
+    addSequential(new CascadingPIDTurn(0, 0.7, 0.02, 0));
+    addSequential(new CascadingPIDTurn(120, 0.7, 0.02, 0));
+    addSequential(new PurePursuitController(Robot.pathlist.test2Path, 1.2, 4.8, true, true));
+
+
+
+
   }
 }
--- Gravistar Reporgram/Gravistar/src/main/java/frc/robot/tools/pathTools/PathList.java
+++ Gravistar Reporgram/Gravistar/src/main/java/frc/robot/tools/pathTools/PathList.java
@@ -16,10 +16,11 @@ import jaci.pathfinder.Waypoint;
 public class PathList {
   private File testFile1 = new File("/home/lvuser/deploy/2HatchAuto1.pf1.csv");
   private File testFile2 = new File("/home/lvuser/deploy/2HatchAuto2.pf1.csv");
+  private File testFile3 = new File("/home/lvuser/deploy/2HatchAuto3.pf1.csv");
 
   public PathSetup test2Path;
   public PathSetup test1Path;
-
+  public PathSetup test3Path;
 
    //remember that for all paths if the first point is at (0,0,0) for some reason the end y value is revesred in the coordinate plane
   //for example for a path from (x,y,h) to (0,0,0) a path that goes from (0,0,0) to (x,y,h) would look the same but for one you would 
@@ -27,6 +28,8 @@ public class PathList {
   public PathList() {
     test1Path = new PathSetup(testFile1, true);
     test2Path = new PathSetup(testFile2, true);
+    test3Path = new PathSetup(testFile3, false);
+
 	}
     
   
